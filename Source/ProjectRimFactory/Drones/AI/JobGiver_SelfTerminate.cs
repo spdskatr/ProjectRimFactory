349706d940606d3b7d5723c4cd5d7a490365c819
spdskatr <tunan.shi@gmail.com>
2018-05-27 20:24:09 +1000

    Drone research stuff

---
+++ Source/ProjectRimFactory/Drones/AI/JobGiver_SelfTerminate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using Verse.AI;
+
+namespace ProjectRimFactory.Drones.AI
+{
+    public class JobDriver_SelfTerminate : JobDriver
+    {
+        public override bool TryMakePreToilReservations()
+        {
+            return true;
+        }
+
+        protected override IEnumerable<Toil> MakeNewToils()
+        {
+            yield return new Toil()
+            {
+                initAction = () =>
+                {
+                    pawn.DeSpawn();
+                },
+                defaultCompleteMode = ToilCompleteMode.Instant
+            };
+        }
+    }
+}
